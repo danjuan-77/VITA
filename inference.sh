37a2a51c445712819e0426838739210c415ed59d
danjuan-77 <youngertutu@gmail.com>
2025-04-24 14:09:59 +0800

    Update inference demo sh

---
+++ inference.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+
+python video_audio_demo.py \
+    --model_path "/share/nlp/tuwenming/models/VITA-MLLM/VITA-1.5" \
+    --image_path asset/vita_newlog.jpg \
+    --model_type qwen2p5_instruct \
+    --conv_mode qwen2p5_instruct \
+    --question "Describe this images."
+
+python video_audio_demo.py \
+   --model_path "/share/nlp/tuwenming/models/VITA-MLLM/VITA-1.5" \
+    --image_path asset/vita_newlog.png \
+    --model_type qwen2p5_instruct \
+    --conv_mode qwen2p5_instruct \
+    --audio_path asset/q1.wav
+
