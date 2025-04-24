0df9be09c702780b573cbbf65ae248547a29bff3
danjuan-77 <youngertutu@gmail.com>
2025-04-24 15:13:53 +0800

    fix error

--- inference.sh
+++ inference.sh
@@ -11,7 +11,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 python video_audio_demo.py \
     --model_path "/share/nlp/tuwenming/models/VITA-MLLM/VITA-1.5" \
-    --image_path asset/vita_newlog.png \
+    --image_path asset/vita_newlog.jpg \
     --model_type qwen2p5_instruct \
     --conv_mode qwen2p5_instruct \
     --audio_path asset/q1.wav
