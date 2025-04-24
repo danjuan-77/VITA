861ec9e98f8a949d70e530d0a32aaeb910473aca
danjuan-77 <youngertutu@gmail.com>
2025-04-24 14:51:00 +0800

    update sh

--- inference.sh
+++ inference.sh
@@ -2,12 +2,12 @@
 
 export CUDA_VISIBLE_DEVICES=0
 
-python video_audio_demo.py \
-    --model_path "/share/nlp/tuwenming/models/VITA-MLLM/VITA-1.5" \
-    --image_path asset/vita_newlog.jpg \
-    --model_type qwen2p5_instruct \
-    --conv_mode qwen2p5_instruct \
-    --question "Describe this images."
+# python video_audio_demo.py \
+#     --model_path "/share/nlp/tuwenming/models/VITA-MLLM/VITA-1.5" \
+#     --image_path asset/vita_newlog.jpg \
+#     --model_type qwen2p5_instruct \
+#     --conv_mode qwen2p5_instruct \
+#     --question "Describe this images."
 
 python video_audio_demo.py \
    --model_path "/share/nlp/tuwenming/models/VITA-MLLM/VITA-1.5" \
