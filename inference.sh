976d9d193913dc235960202a0b3446fb3cc9c276
danjuan-77 <youngertutu@gmail.com>
2025-04-26 18:00:57 +0800

    test audio+text

--- inference.sh
+++ inference.sh
@@ -9,10 +9,16 @@ export CUDA_VISIBLE_DEVICES=0
 #     --conv_mode qwen2p5_instruct \
 #     --question "Describe this images."
 
-python video_audio_demo.py \
-    --model_path "/share/nlp/tuwenming/models/VITA-MLLM/VITA-1.5" \
-    --image_path asset/vita_newlog.jpg \
-    --model_type qwen2p5_instruct \
-    --conv_mode qwen2p5_instruct \
-    --audio_path asset/q1.wav
+# python video_audio_demo.py \
+#     --model_path "/share/nlp/tuwenming/models/VITA-MLLM/VITA-1.5" \
+#     --image_path asset/vita_newlog.jpg \
+#     --model_type qwen2p5_instruct \
+#     --conv_mode qwen2p5_instruct \
+#     --audio_path asset/q1.wav
 
+python video_audio_demo.py \
+  --model_path "/share/nlp/tuwenming/models/VITA-MLLM/VITA-1.5" \
+  --audio_path  "/share/nlp/tuwenming/projects/MiniCPM-o/assets/input_examples/audio_understanding.mp3" \
+  --model_type  qwen2p5_instruct \
+  --conv_mode   qwen2p5_instruct \
+  --question    "Please describe the content of this audio."
