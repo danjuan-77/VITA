f50e6267ef474535d31e34f9b5dc5279f7c145c5
danjuan-77 <youngertutu@gmail.com>
2025-05-02 14:51:39 +0800

    11

--- eval.sh
+++ eval.sh
@@ -22,12 +22,12 @@ CONV_MODE="qwen2p5_instruct"
 # # Test 2: Text + Audio
 # # --------------------------
 # # Provide an audio file and a question.
-# python video_audio_demo.py \
-#   --model_path "$MODEL_PATH" \
-#   --model_type "$MODEL_TYPE" \
-#   --conv_mode "$CONV_MODE" \
-#   --question "Describe the audio." \
-#   --audio_path "/share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LAQA/input/wavs/beats_counting/00000_0.wav"
+python video_audio_demo.py \
+  --model_path "$MODEL_PATH" \
+  --model_type "$MODEL_TYPE" \
+  --conv_mode "$CONV_MODE" \
+  --question "Describe the audio." \
+  --audio_path "/share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LAQA/input/wavs/beats_counting/00000_0.wav"
 
 # # --------------------------
 # # Test 3: Text + Video
@@ -53,8 +53,8 @@ CONV_MODE="qwen2p5_instruct"
 #   --audio_path "/share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVH/input/wavs/00000.wav"
 
 
-python eval.py \
-  --model_path "$MODEL_PATH" \
-  --model_type "$MODEL_TYPE" \
-  --conv_mode "$CONV_MODE" \
-  --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LAQA
+# python eval.py \
+#   --model_path "$MODEL_PATH" \
+#   --model_type "$MODEL_TYPE" \
+#   --conv_mode "$CONV_MODE" \
+#   --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LAQA
